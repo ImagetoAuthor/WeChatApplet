dcf01d7d168c8f4bc68b293f489f5e20d7a3c015
979391398 <54517176+979391398@users.noreply.github.com>
2020-12-01 09:48:03 +0800

    addselect

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    array: [{
+      st_name: "查询画作",
+      }, {
+      st_name: "自我检验",
+      }],
+      index: 0,
   },
 
   /**
@@ -71,4 +76,65 @@ Page({
   }
 
 
+<<<<<<< Updated upstream
+=======
+  //添加上传图片
+  chooseImageTap: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#00000",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+  },
+  // 图片本地路径
+  chooseWxImage: function (type) {
+    var that = this;
+    var imgsPaths = that.data.imgs;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res.tempFilePaths[0]);
+        that.upImgs(res.tempFilePaths[0], 0) //调用上传方法
+      }
+    }) 
+  },
+  //上传服务器
+  upImgs: function (imgurl, index) {
+    var that = this;
+    wx.uploadFile({
+      url: 'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxx',//
+      filePath: imgurl,
+      name: 'file',
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+      formData: null,
+      success: function (res) {
+        console.log(res) //接口返回网络路径
+        var data = JSON.parse(res.data)
+          that.data.picPaths.push(data['msg'])
+          that.setData({
+            picPaths: that.data.picPaths
+          })
+          console.log(that.data.picPaths)
+      }
+    })
+  },
+  listenerPickerSelected: function (e) {
+    //改变index值，通过setData()方法重绘界面
+    this.setData({
+    index: e.detail.value
+    });
+    },
+>>>>>>> Stashed changes
 })
